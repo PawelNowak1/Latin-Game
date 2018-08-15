a39abfdabda5b84a10d0fa6d7e68d39108205d16
Pawel Nowak <p.n98@interia.pl>
2018-08-15 13:02:38 +0200

    created new activity v2

--- app/src/main/java/com/latingame/pawel/latingame/MenuActivity.java
+++ app/src/main/java/com/latingame/pawel/latingame/MenuActivity.java
@@ -2,12 +2,41 @@ package com.latingame.pawel.latingame;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MenuActivity extends AppCompatActivity {
+
+    private Button playButton, howToPlayButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        init();
     }
+
+    private void init(){
+        initButtons();
+    }
+
+    private void initButtons(){
+        playButton = findViewById(R.id.playBtn);
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // TODO: new howManyplayersaCtivity
+            }
+        });
+
+        howToPlayButton = findViewById(R.id.howToPlayBtn);
+        howToPlayButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // TODO: howToPlayActivity
+            }
+        });
+    }
+
+
 }
